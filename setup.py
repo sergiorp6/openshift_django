38dc56aeab7947de02eb3999b2e55d86d693a8ae
Sergio Rodriguez <sergiorp6@gmail.com>
2014-09-25 20:29:57 +0200

    Replace Django 1.6 by django 1.7 and added .gitignore

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='MyDjangoApp',
       author='Yourname',
       author_email='your_email@somewhere.com',
       url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['django==1.6','django-crispy-forms'],
+      install_requires=['django==1.7','django-crispy-forms'],
      )
